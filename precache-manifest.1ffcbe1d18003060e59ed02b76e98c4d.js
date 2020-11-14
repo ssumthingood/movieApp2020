0d99f15a717a7816c6ff70914d64ad9d874687f3
ssojuchick <ssojuchick@naver.com>
2020-11-14 23:34:32 +0900

    Updates

---
+++ precache-manifest.1ffcbe1d18003060e59ed02b76e98c4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91d75f2c37cdebab948722195c35d43",
+    "url": "/movieApp2020/index.html"
+  },
+  {
+    "revision": "d58b1087d1d693885ca1",
+    "url": "/movieApp2020/static/css/main.9be9fb95.chunk.css"
+  },
+  {
+    "revision": "167264ec58f7481e92b0",
+    "url": "/movieApp2020/static/js/2.c4738061.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/movieApp2020/static/js/2.c4738061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d58b1087d1d693885ca1",
+    "url": "/movieApp2020/static/js/main.9b5feb13.chunk.js"
+  },
+  {
+    "revision": "bcf18d6cb4b64994a81f",
+    "url": "/movieApp2020/static/js/runtime-main.95511cd7.js"
+  }
+]);
